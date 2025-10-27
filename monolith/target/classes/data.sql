8e50c58a1582c604546b90e45757b514b28e19d5
Daniel Alejandro AV☄️ <daniel.acero@mail.escuelaing.edu.co>
2025-10-26 20:02:01 -0500

    chore: .gitignore y dejar de trackear target/

--- monolith/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
--- Crea si no existen (H2)
-MERGE INTO users (id, email, username) KEY(id) VALUES (1, 'alice@example.com', 'alice');
-MERGE INTO stream_topic (id, name) KEY(id) VALUES (1, 'general');
